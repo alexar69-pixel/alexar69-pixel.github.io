877e768469bbb2f9d06a505b68188b1289133d8e
alexar69-pixel <alexar69@gmail.com>
2026-05-28 09:48:21 +0200

    build(rss): automatizar generación de RSS feed al crear posts y actualizar rss.xml

--- scripts/new-blog-post.ps1
+++ scripts/new-blog-post.ps1
@@ -37,5 +37,37 @@ $updated = @($posts | Where-Object { $_.slug -ne $slug }) + @($newItem)
 $updated = $updated | Sort-Object { [datetime]$_.date } -Descending
 $updated | ConvertTo-Json -Depth 5 | Set-Content -Encoding UTF8 -LiteralPath $postsJsonPath
 
+# Regenerar blog/rss.xml automáticamente
+$rssPath = Join-Path $repo 'blog\rss.xml'
+$buildDate = (Get-Date).ToUniversalTime().ToString("r")
+
+$xmlItems = ""
+foreach ($p in $updated) {
+  $pDate = [DateTime]::Parse($p.date).ToString("r")
+  $xmlItems += "    <item>`n"
+  $xmlItems += "      <title>$($p.title)</title>`n"
+  $xmlItems += "      <link>https://alexanderarmentia.com/blog/post.html?slug=$($p.slug)</link>`n"
+  $xmlItems += "      <guid>https://alexanderarmentia.com/blog/post.html?slug=$($p.slug)</guid>`n"
+  $xmlItems += "      <pubDate>$pDate</pubDate>`n"
+  $xmlItems += "      <description>$($p.excerpt)</description>`n"
+  $xmlItems += "    </item>`n"
+}
+
+$rssContent = @"
+<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0">
+  <channel>
+    <title>Blog de Alexander Armentia</title>
+    <link>https://alexanderarmentia.com/blog/</link>
+    <description>Resiliencia, tecnología y liderazgo técnico</description>
+    <language>es-es</language>
+    <lastBuildDate>$buildDate</lastBuildDate>
+$xmlItems  </channel>
+</rss>
+"@
+
+$rssContent | Set-Content -Encoding UTF8 -LiteralPath $rssPath
+
 Write-Output "Post created: $slug"
 Write-Output "File: $postFile"
+Write-Output "RSS Feed updated: $rssPath"
